e5eec215c2d738dba4de25f27fd8adc4d34f5e30
luisalbertodev <luisalbertodev@gmail.com>
2021-10-13 11:00:40 -0500

    added component InputWithHidden

---
+++ src/components/Core/InputWithHidden/InputWithHidden.tsx
@@ -0,0 +1,76 @@
+import { useState, FC, MouseEvent } from 'react';
+import { useController } from 'react-hook-form';
+
+import { Col } from 'react-flexbox-grid';
+
+import IconButton from '@mui/material/IconButton';
+import TextField, { BaseTextFieldProps } from '@mui/material/TextField';
+
+import InputAdornment from '@mui/material/InputAdornment';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
+interface IProps extends BaseTextFieldProps {
+  xs?: number;
+  md?: number;
+  name: string;
+  control: any;
+  defaultValue?: string;
+  label: string;
+}
+
+const InputWithHidden: FC<IProps> = ({
+  size = 'medium',
+  xs = 12,
+  md = 6,
+  name,
+  control,
+  defaultValue = '',
+  label
+}) => {
+  const [isShowPassword, setShowPassword] = useState<boolean>(false);
+
+  const {
+    field: { ref, ...inputProps },
+    fieldState: { invalid, error }
+  } = useController({ name, control, defaultValue });
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!isShowPassword);
+  };
+
+  const handleMouseDownPassword = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
+  const endAdornment = (
+    <InputAdornment position="end">
+      <IconButton
+        aria-label={`input for ${name}`}
+        onClick={handleClickShowPassword}
+        onMouseDown={handleMouseDownPassword}
+        edge="end"
+      >
+        {isShowPassword ? <VisibilityOff /> : <Visibility />}
+      </IconButton>
+    </InputAdornment>
+  );
+
+  return (
+    <Col xs={xs} md={md} className="mt3 mb3">
+      <TextField
+        error={invalid}
+        label={label}
+        type={isShowPassword ? 'text' : 'password'}
+        helperText={error?.message}
+        size={size}
+        className="w-100"
+        InputProps={{ endAdornment }}
+        {...inputProps}
+        inputRef={ref}
+      />
+    </Col>
+  );
+};
+
+export default InputWithHidden;
