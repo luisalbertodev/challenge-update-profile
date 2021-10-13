c1c64c0851565297ce41672319e892c1f02f6ef6
luisalbertodev <luisalbertodev@gmail.com>
2021-10-13 11:00:21 -0500

    added component Input

---
+++ src/components/Core/Input/Input.tsx
@@ -0,0 +1,55 @@
+import { FC } from 'react';
+import { useController } from 'react-hook-form';
+
+import { Col } from 'react-flexbox-grid';
+import TextField, { BaseTextFieldProps } from '@mui/material/TextField';
+
+interface IProps extends BaseTextFieldProps {
+  xs?: number;
+  md?: number;
+  name: string;
+  control: any;
+  defaultValue?: string;
+  label: string;
+  hasSpacing?: boolean;
+  className?: string;
+}
+
+const Input: FC<IProps> = ({
+  size = 'medium',
+  xs = 12,
+  md = 6,
+  name,
+  control,
+  defaultValue = '',
+  label,
+  type,
+  hasSpacing = true,
+  className
+}) => {
+  const {
+    field: { ref, ...inputProps },
+    fieldState: { invalid, error }
+  } = useController({ name, control, defaultValue });
+
+  return (
+    <Col
+      xs={xs}
+      md={md}
+      className={`${hasSpacing ? 'mt3 mb3' : ''} ${className}`}
+    >
+      <TextField
+        error={invalid}
+        label={label}
+        type={type}
+        helperText={error?.message}
+        size={size}
+        inputRef={ref}
+        className="w-100"
+        {...inputProps}
+      />
+    </Col>
+  );
+};
+
+export default Input;
