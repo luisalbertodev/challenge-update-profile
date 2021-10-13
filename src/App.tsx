43397d86ca197a9e43776c2e51d6cbf6fbfad0e3
luisalbertodev <luisalbertodev@gmail.com>
2021-10-13 11:02:16 -0500

    update files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'aos/dist/aos.css';
+import AOS from 'aos';
+import { useEffect, Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useAuth } from 'src/context/AuthContext';
 
-function App() {
+import Loader from 'src/components/Core/Loader';
+import CustomizedSnackbars from 'src/components/Core/CustomizedSnackbars';
+import PrivateRoute from 'src/components/Route/PrivateRoute';
+import PublicRoute from 'src/components/Route/PublicRoute';
+
+// public routes
+const Login = lazy(() => import('src/views/public/Login'));
+const Page404 = lazy(() => import('src/views/public/Page404'));
+
+// routes private
+const Main = lazy(() => import('src/views/private/Main'));
+
+export default function App() {
+  const { isLogged } = useAuth();
+
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Suspense fallback={<Loader />}>
+          <Switch>
+            <PrivateRoute
+              isSignedIn={isLogged}
+              exact
+              path="/"
+              component={Main}
+            />
+            <PublicRoute
+              isSignedIn={isLogged}
+              exact
+              path="/identificate"
+              component={Login}
+            />
+            <Route path="*" component={Page404} />
+          </Switch>
+        </Suspense>
+      </Router>
+      <CustomizedSnackbars />
+    </>
   );
 }
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import 'normalize.css';
+import { render } from 'react-dom';
+import { AuthProvider } from 'src/context/AuthContext';
+import { ThemeProvider } from 'src/context/ThemeContext';
+import { GlobalProvider } from 'src/context/GlobalContext';
+import GlobalStyle from 'src/GlobalStyle';
+import { LanguagesProvider } from 'src/context/LanguagesContext';
+
+import App from 'src/App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+render(
+  <AuthProvider>
+    <LanguagesProvider>
+      <ThemeProvider>
+        <GlobalProvider>
+          <>
+            <GlobalStyle />
+            <App />
+          </>
+        </GlobalProvider>
+      </ThemeProvider>
+    </LanguagesProvider>
+  </AuthProvider>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
+// to log results (for example: reportWebVitals())
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
